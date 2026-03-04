64a7943e0341df79a92fcad62c7e50b6c4889357
Ina Bryzheuskaya <inabri783@gmail.com>
2026-03-03 18:52:13 -0600

    add fetch1 and fetch2 and basic css

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
-// Fetch
+const cards = document.querySelector(".cards"); //container where cards will be placed
+const buttonArtworks = document.querySelector("#button-artworks");
+const buttonArtists = document.querySelector("#button-artists");
+const title = document.querySelector("#title");
 
-fetch("https://api.artic.edu/api/v1/artworks")
+// Fetch 1
 
-.then(function(response){
-    if(!response.ok){
-        throw new Error("Request failed.")
-    }
-    return response.json();    
-})
+buttonArtworks
+    .addEventListener("click", function () {
+        fetch("https://api.artic.edu/api/v1/artworks?limit=10")
+            .then(function (response) {
+                if (!response.ok) {
+                    throw new Error("Request failed.");
+                }
+                return response.json();
+            })
 
-.then(function(data){
-    console.log(data);
-})
+            .then(function (data) {
+                title.textContent = "Artworks"
+                cards.textContent = ""; //Clear container before adding new
+                //Loop
+                for (let i = 0; i < data.data.length; i++) {
+                    //Create a new div
+                    const div = document.createElement("div");
+                    //The text insert inside the div
+                    div.textContent = data.data[i].title;
+                    div.className = "card"; //CSS class add
+                    //The element insert into the DOM
+                    cards.appendChild(div);
+                }
+            })
+            .catch(function (error) {
+                console.log("Error:", error);
+            });
+    });
 
-.catch(function(error){
-    console.log("Error:", error);
-});
+//Fetch 2
+
+buttonArtists
+    .addEventListener("click", function () {
+        fetch("https://api.artic.edu/api/v1/artists?limit=10")
+            .then(function (response) {
+                if (!response.ok) {
+                    throw new Error("Request failed.");
+                }
+                return response.json();
+            })
+
+            .then(function (data) {
+                title.textContent = "Artists";
+                cards.textContent = "";
+                for (let i = 0; i < data.data.length; i++) {
+                    const div = document.createElement("div");
+                    div.textContent = data.data[i].title;
+                    div.className = "card";
+                    cards.appendChild(div);
+                }
+            })
+            .catch(function (error) {
+                console.log("Error:", error);
+            });
+    });
